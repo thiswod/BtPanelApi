6021a0b5e5949cf2ee16da86ea77d2b6ff8eea2b
皮卡超人 <thiswod24@gmail.com>
2025-10-08 15:14:49 +0800

    添加项目文件。

---
+++ BtPanel.cs
@@ -0,0 +1,64 @@
+﻿using BtPanelApi.site;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BtPanelApi
+{
+    /// <summary>
+    /// BtPanel Api
+    /// </summary>
+    /// <param name="BtPanel">面板URL地址</param>
+    /// <param name="BtKey">ApiKey</param>
+    public class BtPanel(string BtPanel,string BtKey)
+    {
+        /// <summary>
+        /// 创建md5 hash
+        /// </summary>
+        /// <param name="Key">Key</param>
+        /// <returns></returns>
+        public string GetMd5Hash(string Key)
+        {
+            var md5 = MD5.Create();
+            var inputBytes = Encoding.ASCII.GetBytes(Key);
+            var hashBytes = md5.ComputeHash(inputBytes);
+            var sb = new StringBuilder();
+            foreach (var t in hashBytes)
+            {
+                sb.Append(t.ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+        /// <summary>
+        /// 创建表单
+        /// </summary>
+        /// <param name="Form"></param>
+        /// <returns></returns>
+        public Dictionary<string,string> CreateForm(Dictionary<string,string>? Form = null)
+        {
+            Form ??= new();
+            var requestTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
+            var requestToken = GetMd5Hash(requestTime + GetMd5Hash(BtKey));
+            Form.Add("request_time", requestTime);
+            Form.Add("request_token", requestToken);
+            return Form;
+        }
+        public HttpRequestClass SendConfig(string action)
+        {
+            HttpRequestClass http = new HttpRequestClass();
+            http.Open(BtPanel + "/config?action=" + action, HttpMethod.Post);
+            var PostData = CreateForm();
+            http.Send(PostData);
+            return http;
+        }
+        public HttpRequestClass SendConfig(string action,Dictionary<string,string> Form)
+        {
+            HttpRequestClass http = new HttpRequestClass();
+            http.Open(BtPanel + "/config?action=" + action, HttpMethod.Post);
+            var PostData = CreateForm(Form);
+            http.Send(PostData);
+            return http;
+        }
+        public Php php => new Php(BtPanel, BtKey);
+    }
+}
---
+++ site/Php.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BtPanelApi.site
+{
+    public class Php(string BtPanel, string BtKey) : BtPanel(BtPanel, BtKey)
+    {
+        /// <summary>
+        /// 获取网站分类分组
+        /// </summary>
+        /// <returns></returns>
+        public List<Types> GetSiteTypes()
+        {
+            try
+            {
+                var Result = SendSite("get_site_types");
+                List<Types> types = EasyJson.ParseJsonObject<List<Types>>(Result.GetResponse().Body);
+                return types;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("获取网站分类分组失败:" + ex.Message);
+                return null;
+            }
+        }
+        /// <summary>
+        /// 获取PHPCli版本
+        /// </summary>
+        public PhpCliVersion GetCliPhpVersion()
+        {
+            var Result = SendConfig("get_cli_php_version");
+            PhpCliVersion PhpCliVersion = EasyJson.ParseJsonObject<PhpCliVersion>(Result.GetResponse().Body);
+            return PhpCliVersion;
+        }
+        /// <summary>
+        /// 设置PHPCli版本
+        /// </summary>
+        /// <param name="php_version">PHP版本 如：74、83、84</param>
+        /// <returns></returns>
+        public PhpCliVersion SetCliPhpVersion(string php_version)
+        {
+            var Result = SendConfig("set_cli_php_version", new Dictionary<string, string>()
+            {
+                ["php_version"] = php_version
+            });
+            PhpCliVersion PhpCliVersion = EasyJson.ParseJsonObject<PhpCliVersion>(Result.GetResponse().Body);
+            return PhpCliVersion;
+        }
+        public HttpRequestClass SendSite(string action)
+        {
+            HttpRequestClass http = new HttpRequestClass();
+            http.Open(BtPanel + "/site?action=" + action, HttpMethod.Post);
+            var PostData = CreateForm();
+            http.Send(PostData);
+            return http;
+        }
+        public HttpRequestClass SendSite(string action, Dictionary<string, string> Form)
+        {
+            HttpRequestClass http = new HttpRequestClass();
+            http.Open(BtPanel + "/site?action=" + action, HttpMethod.Post);
+            var PostData = CreateForm(Form);
+            http.Send(PostData);
+            return http;
+        }
+
+    }
+    public class PhpCliVersion 
+    {
+        public Select Select { get; set; }
+        public List<versions> versions { get; set; }
+    }
+    public class Select
+    {
+        /// <summary>
+        /// 版本号
+        /// </summary>
+        public string version { get; set; }
+        /// <summary>
+        /// 版本名称
+        /// </summary>
+        public string name { get; set; }
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public bool status { get; set; }
+    }
+    public class versions
+    {
+        /// <summary>
+        /// 版本号
+        /// </summary>
+        public string version { get; set; }
+        /// <summary>
+        /// 版本名称
+        /// </summary>
+        public string name { get; set; }
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public bool status { get; set; }
+    }
+    public class Types
+    {
+        /// <summary>
+        /// 分类ID
+        /// </summary>
+        public int id { get; set; }
+        /// <summary>
+        /// 分类名称
+        /// </summary>
+        public string name { get; set; }
+    }
+}
